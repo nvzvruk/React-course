6be288e2356aaeaf082b58e1cff4ac185544fb1d
nvzvruk <nazaruk44@gmail.com>
2018-06-04 20:14:59 +0300

    create clock

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./scss/index.scss"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor() {
+        super();
+        this.state =  {time:new Date()}
+    }
+
+    makeClockLive() {
+        this.setState({
+            time: new Date()
+        })
+    }
+
+    componentWillMount() {
+        setInterval(() => {
+            this.makeClockLive();
+        }, 1000);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Simple digital clock</h1>
+                </header>
+                <div className="clock">{this.state.time.toLocaleTimeString()}</div>
+            </div>
+        );
+    }
 }
 
 export default App;
